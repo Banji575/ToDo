cf00fa037e995122df704c483d33a4a2de923e77
Sergey Churkov <Hotmerch@hotmail.com>
2019-06-27 21:20:01 +0300

    Первичная структура проекта

---
+++ src/components/search-panel.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const SearchPanel = () =>{
+    const searhText = 'Type here to searh...'
+    return <input  placeholder={searhText}/>
+};
+
+export default SearchPanel;
